8072b44bb552bd7db952d77e13a1c820d5ca42ad
Wolfnut88 <sonofwolf88@gmail.com>
2018-08-11 22:20:50 -0400

    attempt to change temp to F

--- script/jquery.simpleWeather.js
+++ script/jquery.simpleWeather.js
@@ -3,7 +3,7 @@
   "use strict";
 
   function getAltTemp(unit, temp) {
-    if(unit === 'f') {
+    if(unit === 'c') {
       return Math.round((5.0/9.0)*(temp-32.0));
     } else {
       return Math.round((9.0/5.0)*temp+32.0);
